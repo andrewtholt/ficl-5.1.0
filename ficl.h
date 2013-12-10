ddc00af1f074c3be2755618625044f55597ccc8a
andrewtholt <andrew_tholt@me.com>
2013-12-10 09:22:35 +0000

    Changed version number.

--- ficl.h
+++ ficl.h
@@ -632,7 +632,7 @@ FICL_PLATFORM_EXTERN void *ficlRealloc(void *p, size_t size);
 /* 
 ** the Good Stuff starts here...
 */
-#define FICL_VERSION    "5.1.0"
+#define FICL_VERSION    "5.2.0"
 
 #if !defined (FICL_PROMPT)
 #define FICL_PROMPT		"ok> "
