56dcc1009e5123bce1e8006176a29136b8b453dd
andrewtholt <andrew_tholt@me.com>
2014-03-17 08:42:22 +0000

    Added switches.

--- build.sh
+++ build.sh
@@ -4,10 +4,30 @@
 
 ARCH=`uname -m`
 
+
+while getopts a:hx: flag; do
+    case $flag in
+        a)
+            ARGS=$OPTARG
+            ;;
+        h)
+            echo "Help."
+            echo "\t-a <makefile args>"
+            echo "\t-h\t\tHelp."
+            echo "\t-x <makefile arch>"
+
+            exit 0
+            ;;
+        x)
+            ARCH=$OPTARG
+            ;;
+    esac
+done
+
 MAKEFILE=Makefile.${ARCH}
 
 if [ -f $MAKEFILE ]; then
-	make -f $MAKEFILE $*
+	make -f $MAKEFILE $ARGS
 else
 	echo "$MAKEFILE does not exist."
 fi
