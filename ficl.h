0d9454efe9ce32c13f0960685ddff86134cff32d
andrewtholt <andrew_tholt@me.com>
2015-02-15 16:09:48 +0000

    small changes

--- ficl.h
+++ ficl.h
@@ -205,6 +205,26 @@ extern "C" {
 
 #define FICL_ROBUST                (0)
 
+#endif /* FICL_WANT_SMALL */
+
+#if !defined(FICL_WANT_SMALL)
+#define FICL_WANT_SMALL          (0)
+#endif
+
+#if FICL_WANT_SMALL
+#define FICL_WANT_SOFTWORDS        (0)
+#define FICL_WANT_FILE             (0)
+#define FICL_WANT_FLOAT            (0)
+#define FICL_WANT_USER             (0)
+#define FICL_WANT_LOCALS           (0)
+#define FICL_WANT_DEBUGGER         (0)
+#define FICL_WANT_OOP              (0)
+#define FICL_WANT_PLATFORM         (1)
+#define FICL_WANT_MULTITHREADED    (0)
+#define FICL_WANT_EXTENDED_PREFIX  (0)
+
+#define FICL_ROBUST                (0)
+
 #endif /* FICL_WANT_MINIMAL */
 
 
