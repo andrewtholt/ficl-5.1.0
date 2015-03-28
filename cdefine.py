70c36b23a7a9f2f2048ed707baa9a69aef7906fa
andrewtholt <andrew_tholt@me.com>
2015-03-28 16:07:25 +0000

    C header definitions to Forth, and simple test header file.

---
+++ cdefine.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+
+import sys
+import re
+
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+def main():
+    pline=""
+    flag = False
+    process=False
+
+    for tmp in sys.stdin:
+        line=tmp.rstrip()
+
+        tmp=line.split("/*")
+
+        line=tmp[0]
+        line = line.expandtabs(1)
+
+        if flag and not line.startswith("#define") and line.endswith("\\"):
+            pline=pline + line
+
+        if flag and not line.endswith("\\"):
+            pline = pline + line
+            flag = False
+            process=True
+
+
+        if line.startswith("#define") and not line.endswith("\\"):
+            flag=False
+            process = True
+            pline = line
+
+        if line.startswith("#define") and line.endswith("\\"):
+            line=line.rstrip("\\")
+            flag = True
+            pline = line
+
+        if process:
+            process=False
+
+            pline = pline.expandtabs(1)
+            pline=re.sub( '\s+', ' ', pline ).strip()
+
+            pline=pline.replace("\\","")
+
+            v=pline.split(" ",2)
+
+            cmd = v[1] + "=" + v[2]
+#            print cmd
+            exec(cmd)
+
+            cmd = "op=" + v[1] 
+#            print cmd
+            exec(cmd)
+
+            if is_number(op):
+                inhex=format(op, '#04X')
+                print inhex + " constant " + v[1]
+            else:
+                print op + " constant " + v[1]
+
+
+
+
+
+main()
---
+++ tst.h
@@ -0,0 +1,9 @@
+
+#define TEST1 1 /* test */
+
+#define TEST2 2
+
+#define TEST4 4
+
+#define TEST3 (TEST4 | TEST1 | \
+        TEST2)
