981da05e63ae794960b5cd19e952184809597b01
Andrew Holt <andrewtholt60@gmail.com>
2015-06-30 11:18:52 +0100

    Changed implemntation of socket-connect, no longer uses get hostbyname.

--- extras.c
+++ extras.c
@@ -2248,9 +2248,7 @@ athSocket(ficlVm * vm)
    */
 /* Stack: addr len port -- flag */
 
-    static void
-athConnect(ficlVm * vm)
-{
+static void athConnect(ficlVm * vm) {
     char           *hostName;
     int             len, port;
     int             tmp;
@@ -2258,12 +2256,42 @@ athConnect(ficlVm * vm)
     int             exitStatus = 0;
     struct sockaddr_in serv_addr;
     struct hostent *hp;
+    int rc;
+
+    struct addrinfo *result = NULL;
+    struct addrinfo hint;
+
+    char portNumber[8];
+
+    memset(&hint, 0 , sizeof(hint));
+
+//    hint.ai_family = AF_UNSPEC;
+    hint.ai_family = AF_INET;
+    hint.ai_socktype = SOCK_STREAM;
 
     port = ficlStackPopInteger(vm->dataStack);
-    len = ficlStackPopInteger(vm->dataStack);
-    hostName = (char *)ficlStackPopPointer(vm->dataStack);
-    hostName[len] = '\0';
 
+    sprintf(portNumber,"%d",port);
+    len = ficlStackPopInteger(vm->dataStack);
+
+    hostName = (char *)ficlStackPopPointer(vm->dataStack);
+//    hostName[len] = '\0';
+    
+//    rc = getaddrinfo(hostName, NULL /*service*/, &hint, &result);
+    rc = getaddrinfo(hostName, portNumber, &hint, &result);
+
+    if( 0 == rc ) {
+        sock1 = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+        if(sock1 < 0) {
+            exitStatus = -1;
+        } else {
+            tmp = connect(sock1, result->ai_addr, result->ai_addrlen );
+            if (tmp < 0)
+                exitStatus = -1;
+        }
+    }
+
+    /*
     hp = (struct hostent *) gethostbyname(hostName);
     memset((char *) &serv_addr, '\0', sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
@@ -2279,6 +2307,7 @@ athConnect(ficlVm * vm)
         if (tmp < 0)
             exitStatus = -1;
     }
+    */
 
     if (exitStatus == 0) {
         ficlStackPushInteger(vm->dataStack, sock1);
@@ -2815,7 +2844,7 @@ static void athModBusNewNet(ficlVm *vm) {
 
     address = ficlStackPopPointer( vm->dataStack );
 
-    address[len]=(char ) NULL;
+    address[len]= (char *)NULL;
 
     ctx = modbus_new_tcp(address, port);
 
